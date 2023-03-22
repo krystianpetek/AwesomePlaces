db533b417268e74c495d0ebcd740f1f4f95c6ec9
Krystian Petek <krystianpetek2@gmail.com>
2023-03-22 19:52:31 +0100

    Added Extension methods for Application and Infrastructure to assign this layers to Api project

---
+++ awesome_places_api/AwesomePlaces.Application/Extensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AwesomePlaces.Application;
+public static class Extensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        return services;
+    }
+}
