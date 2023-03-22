e60e9460139a1b00689a6e2eaa97813a8714b1d9
Krystian Petek <krystianpetek2@gmail.com>
2023-03-22 19:51:55 +0100

    Added identity entities for Core

---
+++ awesome_places_api/AwesomePlaces.Core/Entities/Identity/Role.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AwesomePlaces.Core.Entities.Identity;
+public enum Role
+{
+    Admin,
+    User
+}
---
+++ awesome_places_api/AwesomePlaces.Core/Entities/Identity/User.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AwesomePlaces.Core.Entities.Identity;
+public class User
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Role { get; set; }
+
+    [JsonIgnore]
+    public string? PasswordHash { get; set; }
+}
