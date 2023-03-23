d05b2986c86fedf278995a749e2992b9f6bb422f
Krystian Petek <krystianpetek2@gmail.com>
2023-03-23 19:05:41 +0100

    APP - reorganized files, removed unnecessary comments, files

--- awesome_places/lib/main.dart
+++ awesome_places/lib/main.dart
@@ -1,65 +1,20 @@
+import 'package:awesome_places/screens/home_screen.dart';
 import 'package:awesome_places/themes/application_themes.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const AwesomePlacesApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class AwesomePlacesApp extends StatelessWidget {
+  const AwesomePlacesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Awesome Places',
       theme: ApplicationThemes.darkTheme(),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: const HomeScreen(title: 'Awesome Places'),
     );
   }
 }
--- awesome_places/lib/themes/application_themes.dart
+++ awesome_places/lib/themes/application_themes.dart
@@ -2,27 +2,13 @@ import 'package:awesome_places/themes/color_schemes.dart';
 import 'package:flutter/material.dart';
 
 class ApplicationThemes {
-  // static ThemeData lightTheme() {
-  //   return ThemeData(
-  //     brightness: Brightness.dark,
-  //     fontFamily: 'Georgia',
-  //     textTheme: const TextTheme(
-  //       displayLarge: TextStyle(fontSize: 72, fontWeight: FontWeight.bold),
-  //       titleLarge: TextStyle(
-  //         fontSize: 36,
-  //         fontStyle: FontStyle.italic,
-  //       ),
-  //       bodyMedium: TextStyle(fontSize: 14, fontFamily: 'Hind'),
-  //     ),
-  //   );
-  // }
-
   static ThemeData darkTheme() {
     return ThemeData(
       useMaterial3: true,
       colorScheme: darkColorScheme,
       // primaryColor: const Color.fromRGBO(58, 58, 250, 1.0),
     );
+    // TODO
   }
 
   static ThemeData lightTheme() {
@@ -31,5 +17,14 @@ class ApplicationThemes {
       colorScheme: lightColorScheme,
       // primaryColor: const Color.fromRGBO(58, 58, 250, 1.0),
     );
+    // TODO
   }
+
+  static TextTheme customTextTheme = const TextTheme(
+    bodySmall: TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+    ),
+    // TODO
+  );
 }
