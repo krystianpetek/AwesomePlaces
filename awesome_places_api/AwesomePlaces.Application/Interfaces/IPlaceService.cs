b128736029ae909bbffa3c99bb03b8f765bf0fb1
Krystian Petek <krystianpetek2@gmail.com>
2023-03-23 06:42:40 +0100

    Added 3 methods for PlaceService interface to create, get and get collection with service implementation

--- awesome_places_api/AwesomePlaces.Application/Interfaces/IPlaceService.cs
+++ awesome_places_api/AwesomePlaces.Application/Interfaces/IPlaceService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AwesomePlaces.Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,4 +8,7 @@ using System.Threading.Tasks;
 namespace AwesomePlaces.Application.Interfaces;
 public interface IPlaceService
 {
+    Task<IEnumerable<Place>> GetPlaces();
+    Task<Place> GetPlace(Guid id);
+    Task<Guid> CreatePlace(Place place);
 }
--- awesome_places_api/AwesomePlaces.Infrastructure/Services/PlaceService.cs
+++ awesome_places_api/AwesomePlaces.Infrastructure/Services/PlaceService.cs
@@ -1,4 +1,5 @@
 ﻿using AwesomePlaces.Application.Interfaces;
+using AwesomePlaces.Core.Entities;
 using AwesomePlaces.Core.Repositories;
 
 namespace AwesomePlaces.Infrastructure.Services;
@@ -8,5 +9,21 @@ public class PlaceService : IPlaceService
     public PlaceService(IPlaceRepository placeRepository)
     {
         _placeRepository = placeRepository;
+    }
+
+    public async Task<Guid> CreatePlace(Place place)
+    {
+        return await _placeRepository.CreateAsync(place);
+    }
+
+    public async Task<Place> GetPlace(Guid id)
+    {
+        return await _placeRepository.GetAsync(id);
+
+    }
+
+    public async Task<IEnumerable<Place>> GetPlaces()
+    {
+        return await _placeRepository.GetCollectionAsync();
     }
 }
