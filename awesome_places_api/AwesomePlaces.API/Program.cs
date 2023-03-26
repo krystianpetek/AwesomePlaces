b94879415adc9f261c61f511aad1a6382c983c90
Krystian Petek <krystianpetek2@gmail.com>
2023-03-26 18:23:24 +0200

    API - Added endpoints for register and login user Mapped /users group of endpoints for ensure login and register handling

--- awesome_places_api/AwesomePlaces.API/Program.cs
+++ awesome_places_api/AwesomePlaces.API/Program.cs
@@ -66,7 +66,7 @@ public static class Program
 
         app.UseMiddleware<ErrorHandlingMiddleware>();
 
-        app.MapGroup("/account").MapUsers();
+        app.MapGroup("/users").MapUsers();
         app.MapGroup("/places").MapPlaces();
 
         await app.PlaceEfSeed();
--- awesome_places_api/AwesomePlaces.Api/Routes/UserExtensions.cs
+++ awesome_places_api/AwesomePlaces.Api/Routes/UserExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using AwesomePlaces.Application.Interfaces;
+using AwesomePlaces.Application.Models;
 using AwesomePlaces.Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,7 +9,20 @@ public static class UserExtensions
 {
     public static RouteGroupBuilder MapUsers(this RouteGroupBuilder routeGroupBuilder)
     {
+        routeGroupBuilder.MapPost("register", async ([FromServices] IUsersService usersService, [FromBody] RegisterUserModel registerUserModel, HttpContext context) =>
+        {
+            await usersService.RegisterUser(registerUserModel);
+            return Results.Ok();
 
+        })
+        .WithName("RegisterUser")
+        .WithOpenApi();
+
+        routeGroupBuilder.MapPost("login", async ([FromServices] IUsersService usersService, [FromBody] LoginUserModel loginUserModel, HttpContext context) =>
+        {
+            string token = await usersService.LoginGenerateJwt(loginUserModel);
+            return Results.Ok(token);
+        });
 
         return routeGroupBuilder;
     }
