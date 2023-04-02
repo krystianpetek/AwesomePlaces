279e4830dec9b6d8763625c1dcd0c1477c76429d
Krystian Petek <krystianpetek2@gmail.com>
2023-04-02 19:00:53 +0200

    APP - created full screen list view to scroll among all places one by one horizontally (used pageview builder) WIP because mock rating stars

---
+++ awesome_places/lib/src/features/explore/presentation/place_full_screen_view.dart
@@ -0,0 +1,204 @@
+import 'package:awesome_places/src/features/explore/data/models/explore_state_model.dart';
+import 'package:awesome_places/src/features/explore/data/providers/places_provider.dart';
+import 'package:awesome_places/src/routes/models/routes.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+class PlaceFullScreen extends ConsumerStatefulWidget {
+  const PlaceFullScreen({super.key});
+
+  @override
+  ConsumerState<PlaceFullScreen> createState() => _PlaceFullScreenState();
+}
+
+class _PlaceFullScreenState extends ConsumerState<PlaceFullScreen> {
+  late PageController pageController;
+  late int totalPage;
+
+  @override
+  void initState() {
+    pageController = PageController(
+      initialPage: 0,
+    )..addListener(() {});
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final ExploreStateModel exploreNotifier = ref.watch(exploreProvider);
+    totalPage = exploreNotifier.places.length;
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        shadowColor: Colors.transparent,
+        surfaceTintColor: Colors.transparent,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios_new_outlined),
+          onPressed: () {
+            context.pop();
+          },
+        ),
+      ),
+      body: PageView.builder(
+        scrollDirection: Axis.horizontal,
+        controller: pageController,
+        physics: const RangeMaintainingScrollPhysics(),
+        itemBuilder: (context, index) {
+          return makePage(
+              item: exploreNotifier.places[index],
+              description: exploreNotifier.places[index].description,
+              image: exploreNotifier.places[index].image,
+              page: index + 1,
+              title: exploreNotifier.places[index].name);
+        },
+        itemCount: exploreNotifier.places.length,
+      ),
+    );
+  }
+
+  Widget makePage({image, title, description, page, item}) {
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(image: MemoryImage(image), fit: BoxFit.cover)),
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.bottomRight,
+            stops: const [0.3, 0.9],
+            colors: [
+              Colors.black.withOpacity(.9),
+              Colors.black.withOpacity(.3),
+            ],
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const SizedBox(
+                  height: 40,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.baseline,
+                  textBaseline: TextBaseline.alphabetic,
+                  children: <Widget>[
+                    Text(
+                      page.toString(),
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      '/' + totalPage.toString(),
+                      style: const TextStyle(color: Colors.white, fontSize: 15),
+                    )
+                  ],
+                ),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        title,
+                        style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 50,
+                            height: 1.2,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Container(
+                            margin: const EdgeInsets.only(right: 3),
+                            child: const Icon(
+                              Icons.star,
+                              color: Colors.yellow,
+                              size: 15,
+                            ),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(right: 3),
+                            child: const Icon(
+                              Icons.star,
+                              color: Colors.yellow,
+                              size: 15,
+                            ),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(right: 3),
+                            child: const Icon(
+                              Icons.star,
+                              color: Colors.yellow,
+                              size: 15,
+                            ),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(right: 3),
+                            child: const Icon(
+                              Icons.star,
+                              color: Colors.yellow,
+                              size: 15,
+                            ),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(right: 5),
+                            child: const Icon(
+                              Icons.star,
+                              color: Colors.grey,
+                              size: 15,
+                            ),
+                          ),
+                          const Text(
+                            '4.0',
+                            style: TextStyle(color: Colors.white70),
+                          ),
+                          const Text(
+                            '(2300)',
+                            style:
+                                TextStyle(color: Colors.white38, fontSize: 12),
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 50),
+                        child: Text(
+                          description,
+                          style: TextStyle(
+                              color: Colors.white.withOpacity(.7),
+                              height: 1.9,
+                              fontSize: 15),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      TextButton(
+                        child: const Text(
+                          'READ MORE',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        onPressed: () {
+                          context.pushNamed(Routes.place.name, extra: item);
+                        },
+                      ),
+                      const SizedBox(height: 30),
+                    ],
+                  ),
+                )
+              ]),
+        ),
+      ),
+    );
+  }
+}
