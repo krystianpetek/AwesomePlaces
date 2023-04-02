a89d69fce560b4889ec486b8123947c90d47b1dc
Krystian Petek <krystianpetek2@gmail.com>
2023-04-02 21:57:23 +0200

    App - Added  support to display each place on map using google map api Changed texts, sizing, positioning and styles on place details screen

--- awesome_places/lib/src/features/explore/presentation/place_details_screen.dart
+++ awesome_places/lib/src/features/explore/presentation/place_details_screen.dart
@@ -1,13 +1,23 @@
-import 'dart:ui';
-
 import 'package:awesome_places/src/features/explore/data/models/place.dart';
+import 'package:awesome_places/src/features/explore/presentation/widgets/place_divider.dart';
+import 'package:awesome_places/src/features/explore/presentation/widgets/stars.dart';
 import 'package:awesome_places/src/features/settings/data/themes/color_schemes.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class PlaceDetailsScreen extends StatelessWidget {
-  const PlaceDetailsScreen({Key? key, required this.place}) : super(key: key);
+import 'widgets/place_back_button.dart';
+
+class PlaceDetailsScreen extends StatefulWidget {
+  PlaceDetailsScreen({Key? key, required this.place}) : super(key: key);
   final Place place;
+
+  @override
+  State<PlaceDetailsScreen> createState() => _PlaceDetailsScreenState();
+}
+
+class _PlaceDetailsScreenState extends State<PlaceDetailsScreen> {
+  bool heartClick = false;
+  late GoogleMapController? mapController;
 
   @override
   Widget build(BuildContext context) {
@@ -18,48 +28,20 @@ class PlaceDetailsScreen extends StatelessWidget {
           Container(
             height: MediaQuery.of(context).size.height / 2,
             decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: MemoryImage(place.image), fit: BoxFit.cover)),
+              image: DecorationImage(
+                  image: MemoryImage(widget.place.image),
+                  colorFilter: ColorFilter.mode(
+                    Colors.black.withOpacity(0.1),
+                    BlendMode.darken,
+                  ),
+                  fit: BoxFit.cover),
+            ),
           ),
-          buttonArrow(context),
+          const PlaceBackButton(),
           scroll(),
         ],
       ),
     ));
-  }
-
-  buttonArrow(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: InkWell(
-        onTap: () {
-          context.pop();
-        },
-        child: Container(
-          clipBehavior: Clip.hardEdge,
-          height: 55,
-          width: 55,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(25),
-          ),
-          child: BackdropFilter(
-            filter: ImageFilter.blur(sigmaX: 0, sigmaY: 0),
-            child: Container(
-              height: 55,
-              width: 55,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(25),
-              ),
-              child: const Icon(
-                Icons.arrow_back_ios_new,
-                size: 20,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
   }
 
   scroll() {
@@ -70,148 +52,114 @@ class PlaceDetailsScreen extends StatelessWidget {
         builder: (context, scrollController) {
           return Container(
             padding: const EdgeInsets.symmetric(horizontal: 20),
-            clipBehavior: Clip.hardEdge,
+            clipBehavior: Clip.antiAlias,
             decoration: const BoxDecoration(
-              color: Colors.white,
+              color: Color.fromARGB(255, 30, 30, 30),
               borderRadius: BorderRadius.only(
-                  topLeft: const Radius.circular(20),
-                  topRight: const Radius.circular(20)),
+                  topLeft: Radius.circular(20), topRight: Radius.circular(20)),
             ),
             child: SingleChildScrollView(
               controller: scrollController,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 10, bottom: 25),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 25),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            height: 5,
+                            width: 35,
+                            color: Colors.white54,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Text(
+                      widget.place.name,
+                      style: Theme.of(context).textTheme.headlineMedium!,
+                    ),
+                    Row(
                       children: [
-                        Container(
-                          height: 5,
-                          width: 35,
-                          color: Colors.black12,
+                        Text(
+                          widget.place.address!.country,
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyMedium!
+                              .copyWith(color: SecondaryText),
                         ),
+                        const Spacer(),
+                        IconButton(
+                          onPressed: () {
+                            setState(() {
+                              heartClick = !heartClick;
+                            });
+                          },
+                          icon: Icon(
+                            heartClick ? Icons.favorite : Icons.favorite_border,
+                            color: heartClick ? Colors.red : Colors.white,
+                          ),
+                        ),
+                        const SizedBox(width: 7),
+                        Stars(rating: widget.place.rating),
                       ],
                     ),
-                  ),
-                  Text(
-                    "Cacao Maca Walnut Milk",
-                    style: Theme.of(context).textTheme.headlineMedium,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    "Food .60 min",
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodyMedium!
-                        .copyWith(color: SecondaryText),
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  Row(
-                    children: [
-                      InkWell(
-                        onTap: () {},
-                        child: const CircleAvatar(
-                          radius: 25,
-                          backgroundImage:
-                              AssetImage("assets/images/author.png"),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 5,
-                      ),
-                      Text(
-                        "Elena Shelby",
-                        style: Theme.of(context)
-                            .textTheme
-                            .headlineSmall!
-                            .copyWith(color: mainText),
-                      ),
-                      const Spacer(),
-                      const CircleAvatar(
-                        radius: 25,
-                        backgroundColor: primary,
-                        child: Icon(
-                          Icons.outlined_flag,
-                          color: Colors.white,
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 5,
-                      ),
-                      Text(
-                        "273 Likes",
-                        style: Theme.of(context)
-                            .textTheme
-                            .headlineSmall!
-                            .copyWith(color: mainText),
-                      ),
-                    ],
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 15),
-                    child: Divider(
-                      height: 4,
+                    const PlaceDivider(),
+                    Text(
+                      "Description",
+                      style: Theme.of(context).textTheme.headlineLarge,
                     ),
-                  ),
-                  Text(
-                    "Description",
-                    style: Theme.of(context).textTheme.headlineLarge,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    'Your recipe has been uploaded, you can see it on your profile. Your recipe has been uploaded, you can see it on your',
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodyMedium!
-                        .copyWith(color: SecondaryText),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 15),
-                    child: Divider(
-                      height: 4,
+                    const SizedBox(height: 10),
+                    Text(
+                      widget.place.description,
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyMedium!
+                          .copyWith(color: SecondaryText),
                     ),
-                  ),
-                  Text(
-                    "Ingredients",
-                    style: Theme.of(context).textTheme.headlineLarge,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  ListView.builder(
-                    physics: NeverScrollableScrollPhysics(),
-                    shrinkWrap: true,
-                    itemCount: 3,
-                    itemBuilder: (context, index) => ingredients(context),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 15),
-                    child: Divider(
-                      height: 4,
+                    const PlaceDivider(),
+                    Text(
+                      "Location",
+                      style: Theme.of(context).textTheme.headlineLarge,
                     ),
-                  ),
-                  Text(
-                    "Steps",
-                    style: Theme.of(context).textTheme.headlineLarge,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  ListView.builder(
-                    physics: NeverScrollableScrollPhysics(),
-                    shrinkWrap: true,
-                    itemCount: 3,
-                    itemBuilder: (context, index) => steps(context, index),
-                  ),
-                ],
+                    const SizedBox(height: 10),
+                    ListView.builder(
+                      physics: NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: 3,
+                      itemBuilder: (context, index) => ingredients(context),
+                    ),
+                    const PlaceDivider(),
+                    Text(
+                      "Route",
+                      style: Theme.of(context).textTheme.headlineLarge,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    ListView.builder(
+                      physics: NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: 3,
+                      itemBuilder: (context, index) => steps(context, index),
+                    ),
+                    SizedBox(
+                      height: 500,
+                      child: GoogleMap(
+                        onMapCreated: (controller) {
+                          mapController = controller;
+                        },
+                        initialCameraPosition: CameraPosition(
+                            target: LatLng(widget.place.coordinate!.latitude,
+                                widget.place.coordinate!.longitude),
+                            zoom: 12),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           );
@@ -223,6 +171,7 @@ class PlaceDetailsScreen extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: 10),
       child: Row(
         children: [
+          Text("${widget.place.coordinate!.latitude}"),
           const CircleAvatar(
             radius: 10,
             backgroundColor: Color(0xFFE3FFF8),
---
+++ awesome_places/lib/src/features/explore/presentation/widgets/stars.dart
@@ -0,0 +1,60 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class Stars extends StatelessWidget {
+  final double rating;
+  const Stars({super.key, required this.rating});
+
+  @override
+  Widget build(BuildContext context) {
+    return Builder(
+      builder: (context) {
+        return Row(
+          children: [
+            ...buildStars(),
+            Text(
+              '$rating',
+              style: TextStyle(color: Colors.white70),
+            ),
+            Text(
+              ' (${Random().nextInt(1000)})',
+              style: TextStyle(color: Colors.white38, fontSize: 12),
+            )
+          ],
+        );
+      },
+    );
+  }
+
+  List<Container> buildStars() {
+    List<Container> containers = [];
+
+    for (int i = 1; i <= 5; i++) {
+      if (rating > i) {
+        containers.add(
+          Container(
+            margin: const EdgeInsets.only(right: 3),
+            child: const Icon(
+              Icons.star,
+              color: Colors.yellow,
+              size: 15,
+            ),
+          ),
+        );
+      } else {
+        containers.add(
+          Container(
+            margin: const EdgeInsets.only(right: 5),
+            child: const Icon(
+              Icons.star,
+              color: Colors.grey,
+              size: 15,
+            ),
+          ),
+        );
+      }
+    }
+    return containers;
+  }
+}
