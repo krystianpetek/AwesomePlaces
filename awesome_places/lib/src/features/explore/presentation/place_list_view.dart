c0e9d471c80e78acc1b9e72f0efe7f82e14036d5
Krystian Petek <krystianpetek2@gmail.com>
2023-04-02 19:05:06 +0200

    APP - Removed variables from place list view, added provider for get state about places Added push route to display each item with place to detailed info (redirect to other screen)

--- awesome_places/lib/src/features/explore/presentation/place_list_view.dart
+++ awesome_places/lib/src/features/explore/presentation/place_list_view.dart
@@ -1,20 +1,26 @@
 import 'package:awesome_places/src/features/explore/data/models/place.dart';
+import 'package:awesome_places/src/features/explore/data/providers/places_provider.dart';
 import 'package:awesome_places/src/features/explore/presentation/place_list_view_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class PlaceListView extends StatelessWidget {
-  const PlaceListView({super.key, required this.places});
-  final List<Place> places;
+  const PlaceListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-        itemBuilder: (context, index) {
-          return PlaceListViewItem(place: places[index]);
-        },
-        separatorBuilder: (context, index) {
-          return const SizedBox(height: 30);
-        },
-        itemCount: places.length);
+    return Consumer(
+      builder: (context, ref, child) {
+        final exploreNotifier = ref.watch(exploreProvider);
+        return ListView.separated(
+            itemBuilder: (context, index) {
+              return PlaceListViewItem(place: exploreNotifier.places[index]);
+            },
+            separatorBuilder: (context, index) {
+              return const SizedBox(height: 30);
+            },
+            itemCount: exploreNotifier.places.length);
+      },
+    );
   }
 }
--- awesome_places/lib/src/features/explore/presentation/place_list_view_item.dart
+++ awesome_places/lib/src/features/explore/presentation/place_list_view_item.dart
@@ -1,5 +1,7 @@
 import 'package:awesome_places/src/features/explore/data/models/place.dart';
+import 'package:awesome_places/src/routes/models/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class PlaceListViewItem extends StatefulWidget {
   PlaceListViewItem({super.key, required this.place});
@@ -25,82 +27,90 @@ class _PlaceListViewItemState extends State<PlaceListViewItem> {
           opacity = 0.4;
         });
       },
-      child: Center(
-        child: Column(
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width - 50,
-              height: MediaQuery.of(context).size.width / 2,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  colorFilter: ColorFilter.mode(
-                      Colors.black.withOpacity(opacity), BlendMode.darken),
-                  filterQuality: FilterQuality.high,
-                  opacity: 0.5,
-                  image: MemoryImage(widget.place.image),
-                  fit: BoxFit.cover,
+      child: GestureDetector(
+        onTap: () {
+          context.pushNamed(Routes.place.name, extra: widget.place);
+        },
+        child: Center(
+          child: Column(
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width - 50,
+                height: MediaQuery.of(context).size.width / 2,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    colorFilter: ColorFilter.mode(
+                        Colors.black.withOpacity(opacity), BlendMode.darken),
+                    filterQuality: FilterQuality.high,
+                    opacity: 0.5,
+                    image: MemoryImage(widget.place.image),
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: const BorderRadius.all(Radius.circular(10.0)),
                 ),
-                borderRadius: const BorderRadius.all(Radius.circular(10.0)),
-              ),
-              child: Stack(
-                children: [
-                  Positioned(
-                    bottom: 5,
-                    right: 10,
-                    child: Text(widget.place.address!.country,
-                        style:
-                            Theme.of(context).textTheme.headlineSmall!.copyWith(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w300,
-                                )),
-                  ),
-                  Positioned(
-                    top: 5,
-                    left: 10,
-                    child: Text(widget.place.name,
-                        style: Theme.of(context)
-                            .textTheme
-                            .headlineMedium!
-                            .copyWith(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                            )),
-                  ),
-                  Positioned(
-                      bottom: 10,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      bottom: 5,
+                      right: 10,
+                      child: Text(widget.place.address!.country,
+                          style: Theme.of(context)
+                              .textTheme
+                              .headlineSmall!
+                              .copyWith(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w300,
+                              )),
+                    ),
+                    Positioned(
+                      top: 5,
                       left: 10,
-                      child: Container(
-                          child: ClipRRect(
-                        child: Row(
-                          children: [
-                            const Icon(Icons.location_on_outlined),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text("X: ${widget.place.coordinate!.latitude}"),
-                                Text(
-                                    "Y: ${widget.place.coordinate!.longitude}"),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ))),
-                ],
+                      child: Text(widget.place.name,
+                          style: Theme.of(context)
+                              .textTheme
+                              .headlineMedium!
+                              .copyWith(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              )),
+                    ),
+                    Positioned(
+                        bottom: 10,
+                        left: 10,
+                        child: Container(
+                            child: ClipRRect(
+                          child: Row(
+                            children: [
+                              const Icon(Icons.location_on_outlined),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                      "X: ${widget.place.coordinate!.latitude}"),
+                                  Text(
+                                      "Y: ${widget.place.coordinate!.longitude}"),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ))),
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding:
-                  EdgeInsets.only(top: 10, left: 16, right: 16, bottom: 10),
-              child: Text(
-                widget.place.description,
-                style: const TextStyle(fontSize: 15),
-                textAlign: TextAlign.left,
+              Padding(
+                padding:
+                    EdgeInsets.only(top: 10, left: 16, right: 16, bottom: 10),
+                child: Text(
+                  widget.place.description,
+                  style: const TextStyle(fontSize: 15),
+                  textAlign: TextAlign.left,
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
