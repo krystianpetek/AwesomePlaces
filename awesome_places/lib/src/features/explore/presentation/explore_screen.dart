f39586b44c17d7c5be5973ce9c5762887d383439
Krystian Petek <krystianpetek2@gmail.com>
2023-04-02 06:33:08 +0200

    App - Handled switching between Grid na List view and cached response from API, to prevent fetching data from api when screen refreshed

--- awesome_places/lib/src/features/explore/presentation/explore_screen.dart
+++ awesome_places/lib/src/features/explore/presentation/explore_screen.dart
@@ -1,10 +1,15 @@
+import 'package:awesome_places/src/features/explore/data/models/place.dart';
 import 'package:awesome_places/src/features/explore/data/services/places_service.dart';
+import 'package:awesome_places/src/features/explore/enums/place_view_enum.dart';
+import 'package:awesome_places/src/features/explore/presentation/place_grid_view.dart';
 import 'package:awesome_places/src/features/explore/presentation/place_list_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class ExploreScreen extends ConsumerStatefulWidget {
-  const ExploreScreen({super.key});
+  ExploreScreen({super.key});
+  late List<Place> places = <Place>[];
+  PlaceViewEnum placeViewEnum = PlaceViewEnum.ListView;
 
   @override
   ConsumerState<ExploreScreen> createState() => _ExploreScreenState();
@@ -18,25 +23,63 @@ class _ExploreScreenState extends ConsumerState<ExploreScreen> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
-          SizedBox(
-            height: MediaQuery.of(context).size.height - 150,
-            child: FutureBuilder(
-              future: PlacesService(ref: ref).getPlaces(),
-              builder: (context, snapshot) {
-                switch (snapshot.connectionState) {
-                  case ConnectionState.none:
-                  case ConnectionState.waiting:
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  case ConnectionState.done:
-                    return PlaceListView(places: snapshot?.data);
-                  default:
-                    return Container();
-                }
-              },
-            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              PopupMenuButton(
+                itemBuilder: (context) {
+                  return [
+                    PopupMenuItem(
+                      child: Text("List view"),
+                      onTap: () {
+                        setState(() {
+                          widget.placeViewEnum = PlaceViewEnum.ListView;
+                        });
+                      },
+                    ),
+                    PopupMenuItem(
+                      child: Text("Grid view"),
+                      onTap: () {
+                        setState(() {
+                          widget.placeViewEnum = PlaceViewEnum.GridView;
+                        });
+                      },
+                    )
+                  ];
+                },
+              ),
+            ],
           ),
+          Expanded(
+              child: (widget.places.isEmpty)
+                  ? SizedBox(
+                      child: FutureBuilder(
+                        future: PlacesService(ref: ref).getPlaces(),
+                        builder: (context, snapshot) {
+                          switch (snapshot.connectionState) {
+                            case ConnectionState.none:
+                            case ConnectionState.waiting:
+                              return const Center(
+                                child: CircularProgressIndicator(),
+                              );
+                            case ConnectionState.done:
+                              widget.places = snapshot.data!;
+                              return Expanded(
+                                  child: widget.placeViewEnum ==
+                                          PlaceViewEnum.ListView
+                                      ? PlaceListView(places: snapshot.data!)
+                                      : PlaceGridView(places: snapshot.data!));
+                            default:
+                              return Container();
+                          }
+                        },
+                      ),
+                    )
+                  : SizedBox(
+                      height: MediaQuery.of(context).size.height - 150,
+                      child: widget.placeViewEnum == PlaceViewEnum.ListView
+                          ? PlaceListView(places: widget.places)
+                          : PlaceGridView(places: widget.places))),
         ],
       ),
     );
